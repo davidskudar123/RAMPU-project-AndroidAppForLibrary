10d3321b81d336979bcde5221a73c3c15c27019a
mstura-FOI <mstura21@student.foi.hr>
2023-11-04 15:58:48 +0100

    Napravljeni bottom navigation bar

--- Software/Scriptify/app/src/main/java/com/example/scriptify/hr/MainActivity.kt
+++ Software/Scriptify/app/src/main/java/com/example/scriptify/hr/MainActivity.kt
@@ -6,13 +6,11 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textTest : TextView
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textTest = findViewById(R.id.text_Test)
 
-        textTest.text = "Test"
     }
 }
