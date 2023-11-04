e0295dc05246c473ce238f581b6aa287a9292a08
mstura-FOI <mstura21@student.foi.hr>
2023-11-04 18:41:43 +0100

    Kreirani su svi potrebni fragmenti- u main activity nije potrebno dodavati kod samo se programiraju fragmenti

--- Software/Scriptify/app/src/main/java/fragments/ProfileFragment.kt
+++ Software/Scriptify/app/src/main/java/fragments/ProfileFragment.kt
@@ -1,9 +1,14 @@
 package fragments
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.scriptify.hr.R
 
 class ProfileFragment : Fragment(R.layout.profile_activity) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
 }
