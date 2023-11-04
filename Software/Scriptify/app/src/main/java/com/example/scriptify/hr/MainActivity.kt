c82fba864070e12b7f11c4dbf16f6a20cb0127b9
mstura-FOI <mstura21@student.foi.hr>
2023-11-04 17:00:04 +0100

    Napravljeni bottom navigation bar

--- Software/Scriptify/app/src/main/java/com/example/scriptify/hr/MainActivity.kt
+++ Software/Scriptify/app/src/main/java/com/example/scriptify/hr/MainActivity.kt
@@ -3,16 +3,41 @@ package com.example.scriptify.hr
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import fragments.MyBooksFragment
+import fragments.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var bottomAppBar: BottomNavigationView
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val profileFragment : ProfileFragment = ProfileFragment()
+        val myBooksFragment: MyBooksFragment = MyBooksFragment()
+
+        bottomAppBar = findViewById(R.id.bottom_navigation)
+        setCurrentFragment(profileFragment)
+
+        bottomAppBar.setOnItemSelectedListener {item->
+            when(item.itemId){
+                R.id.profile -> { setCurrentFragment(profileFragment)
+                true}
+                R.id.my_books -> { setCurrentFragment(myBooksFragment)
+                    true}
+                else -> false
+            }
+
+        }
+
 
     }
+    public fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flLayout,fragment)
+            commit()
+        }
 
 }
---
+++ Software/Scriptify/app/src/main/java/fragments/MyBooksFragment.kt
@@ -0,0 +1,7 @@
+package fragments
+
+import androidx.fragment.app.Fragment
+import com.example.scriptify.hr.R
+
+class MyBooksFragment: Fragment(R.layout.my_books_fragment) {
+}
