9df543ea2c13ac7ff9b536448748e5d2aa025530
mstura-FOI <mstura21@student.foi.hr>
2023-11-05 17:09:30 +0100

    Login funkcija u obradi

---
+++ Software/Scriptify/app/src/main/java/com/example/scriptify/hr/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.scriptify.hr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
