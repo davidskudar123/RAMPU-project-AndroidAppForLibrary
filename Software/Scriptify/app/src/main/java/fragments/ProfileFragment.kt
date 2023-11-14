c64d0a53da2e2472f189099355fc62114581ffc2
David Škudar <dskudar20@foi.hr>
2023-11-14 06:46:22 +0100

    Sada radi dohvacanje iznosa novca te se moze dodavati iznos novaca, samo treba doraditi da se nakon dodavanja osvjezi profilFragment.

--- Software/Scriptify/app/src/main/java/fragments/ProfileFragment.kt
+++ Software/Scriptify/app/src/main/java/fragments/ProfileFragment.kt
@@ -3,11 +3,14 @@ package fragments
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -33,6 +36,12 @@ class ProfileFragment(id:Int) : Fragment(R.layout.profile_activity) {
     lateinit var password: EditText
     lateinit var mail: EditText
     lateinit var profile_button: Button
+    ////novci
+    lateinit var money: TextView
+    lateinit var openMoneyDialog: Button
+    lateinit var add_money: Button
+    lateinit var Close: Button
+    lateinit var input_money_amount: EditText
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,15 +56,25 @@ class ProfileFragment(id:Int) : Fragment(R.layout.profile_activity) {
         password = view.findViewById(R.id.password_profile)
         mail = view.findViewById(R.id.mail_profile)
         profile_button = view.findViewById(R.id.profileUpdate)
-        loadData(first_name, last_name, address, mail,username,password)
+        //dodavanje novaca
+        money = view.findViewById(R.id.money_profile)
+        openMoneyDialog = view.findViewById(R.id.btn_openMoneyDialog)
+        add_money = view.findViewById(R.id.btn_add_money)
+        Close = view.findViewById(R.id.btn_Close)
+        input_money_amount = view.findViewById(R.id.input_money_amount)
+
+        loadData(first_name, last_name, address, mail,username,password, money)
 
         profile_button.setOnClickListener{
             saveUserData(first_name, last_name, address, username, password, mail);
         }
+        openMoneyDialog.setOnClickListener{
+            showDialogAddMoney()
+        }
         return view
     }
 
-    public fun loadData(first_name: EditText, last_name: EditText, address: EditText, mail: EditText,username: EditText, password: EditText){
+    public fun loadData(first_name: EditText, last_name: EditText, address: EditText, mail: EditText,username: EditText, password: EditText, money: TextView){
         viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Default){
             try {
 
@@ -72,6 +91,7 @@ class ProfileFragment(id:Int) : Fragment(R.layout.profile_activity) {
                         mail.setText(user[0].email)
                         password.setText(user[0].password)
                         username.setText(user[0].username)
+                        money.setText(user[0].money.toString())
                     }
 
                 }
@@ -114,5 +134,67 @@ class ProfileFragment(id:Int) : Fragment(R.layout.profile_activity) {
         }
 
     }
+    private fun showDialogAddMoney(){
+        val dialog_add_money = view?.findViewById<FrameLayout>(R.id.dialog_add_money)
+        dialog_add_money?.visibility = View.VISIBLE
 
+        add_money.setOnClickListener{
+
+            if(!TextUtils.isEmpty(input_money_amount.text)){
+
+                var updatedMoney:Int
+                var moneyOfUser = money.text.toString()
+                var inputMoney = input_money_amount.text.toString()
+                if(moneyOfUser.isNotEmpty() && inputMoney.isNotEmpty()){
+                    val courutine =  viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Default) {
+                        try {
+                            var moneyOfUserInt = moneyOfUser.toInt()
+                            var inputMoneyInt = inputMoney.toInt()
+
+                            updatedMoney = moneyOfUserInt + inputMoneyInt
+                            val jsonConverter = JsonConverter()
+                            val jsonBody = jsonConverter.UserMoneyToJsonConverter(Id, updatedMoney)
+
+                            val httpRequestManager = HttpRequestManager()
+
+                            val success = httpRequestManager.updateUserMoney(jsonBody, Id)
+                            launch(Dispatchers.Main) {
+                                if (success) {
+                                    dialog_add_money?.visibility = View.GONE
+                                    Toast.makeText(
+                                        requireContext(),
+                                        "Podaci ažurirani",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                } else {
+                                    Toast.makeText(
+                                        requireContext(),
+                                        "Greška pri ažuriranju podataka",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+
+                        } catch (e: NumberFormatException) {
+                            Toast.makeText(
+                                requireContext(),
+                                "Unos nije ispravan broj",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+                else{
+                    Toast.makeText(requireContext(), "Niste unjeli iznos!", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            else{
+                Toast.makeText(requireContext(), "Unesite ispravan iznos novca!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        Close.setOnClickListener{
+            dialog_add_money?.visibility = View.GONE
+        }
+    }
 }
