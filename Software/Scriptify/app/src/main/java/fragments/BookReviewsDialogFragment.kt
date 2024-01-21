a1a734beaebd0d944b4bc1603c75813f8c181f66
mstura <mstura21@student.foi.hr>
2024-01-21 20:04:38 +0100

    Napravljen book reviews dialog za pregled reviews, knjiga (template)

---
+++ Software/Scriptify/app/src/main/java/fragments/BookReviewsDialogFragment.kt
@@ -0,0 +1,7 @@
+package fragments
+
+import androidx.fragment.app.DialogFragment
+import com.example.scriptify.hr.R
+
+class BookReviewsDialogFragment() : DialogFragment(R.layout.book_reviews_dialog) {
+}
