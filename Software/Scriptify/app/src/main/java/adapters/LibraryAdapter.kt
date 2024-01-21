55753572a48bf49cb944abb8ef129786ad550e64
Luka Krševan Nižić <lnizic21@foi.hr>
2024-01-21 20:50:38 +0100

    Dodan interface za click

--- Software/Scriptify/app/src/main/java/adapters/LibraryAdapter.kt
+++ Software/Scriptify/app/src/main/java/adapters/LibraryAdapter.kt
@@ -9,9 +9,13 @@ import androidx.recyclerview.widget.RecyclerView
 import blueprints.Library
 import com.example.scriptify.hr.R
 
-class LibraryAdapter(private val libraries: List<Library>) :
-    RecyclerView.Adapter<LibraryAdapter.LibraryViewHolder>() {
+class LibraryAdapter(private val libraries: List<Library>,
+                     private val onLibraryClickListener: OnLibraryClickListener
+) : RecyclerView.Adapter<LibraryAdapter.LibraryViewHolder>() {
 
+    interface OnLibraryClickListener {
+        fun onLibraryClick(library: Library)
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LibraryViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_library, parent, false)
@@ -21,6 +25,9 @@ class LibraryAdapter(private val libraries: List<Library>) :
     override fun onBindViewHolder(holder: LibraryViewHolder, position: Int) {
         val library = libraries[position]
         holder.bind(library)
+        holder.itemView.setOnClickListener {
+            onLibraryClickListener.onLibraryClick(library)
+        }
     }
 
     override fun getItemCount(): Int = libraries.size
