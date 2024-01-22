02fae49c0c99217ce96ce834a6dbe2ec004ff158
Luka Krševan Nižić <lnizic21@foi.hr>
2024-01-22 17:20:34 +0100

    Izbrisan click listener za mock datu kako bi se prikazale knjižare

--- Software/Scriptify/app/src/main/java/adapters/LibraryAdapter.kt
+++ Software/Scriptify/app/src/main/java/adapters/LibraryAdapter.kt
@@ -9,8 +9,35 @@ import androidx.recyclerview.widget.RecyclerView
 import blueprints.Library
 import com.example.scriptify.hr.R
 
-class LibraryAdapter(private val libraries: List<Library>,
-                     private val onLibraryClickListener: OnLibraryClickListener
+
+class LibraryAdapter(private val libraries: List<Library>) : RecyclerView.Adapter<LibraryAdapter.LibraryViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LibraryViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_library, parent, false)
+        return LibraryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: LibraryViewHolder, position: Int) {
+        val library = libraries[position]
+        holder.bind(library)
+    }
+
+    override fun getItemCount(): Int = libraries.size
+
+    class LibraryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val libraryNameTextView: TextView = itemView.findViewById(R.id.libraryNameTextView)
+
+        fun bind(library: Library) {
+            libraryNameTextView.text = library.ime
+            // Bind other views with data if needed
+        }
+    }
+}
+
+
+/*class LibraryAdapter(private val libraries: List<Library>,
+                 //    private val onLibraryClickListener: OnLibraryClickListener
 ) : RecyclerView.Adapter<LibraryAdapter.LibraryViewHolder>() {
 
     interface OnLibraryClickListener {
@@ -26,7 +53,7 @@ class LibraryAdapter(private val libraries: List<Library>,
         val library = libraries[position]
         holder.bind(library)
         holder.itemView.setOnClickListener {
-            onLibraryClickListener.onLibraryClick(library)
+            //onLibraryClickListener.onLibraryClick(library)
         }
     }
 
@@ -41,3 +68,4 @@ class LibraryAdapter(private val libraries: List<Library>,
         }
     }
 }
+*/
