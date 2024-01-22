e13b2866ef1c50a2992b91ba17dee23eba83b6e8
David Škudar <dskudar20@foi.hr>
2024-01-22 22:53:25 +0100

    Azurirana baza.

--- Software/Scriptify/db/scriptify_db.sql
+++ Software/Scriptify/db/scriptify_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 21, 2024 at 06:39 PM
+-- Generation Time: Jan 22, 2024 at 10:53 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -45,9 +45,12 @@ INSERT INTO `knjige` (`idKnjige`, `naziv_knjige`, `autor`, `Description`) VALUES
 (16907856, 'Ana Karenjina', 'Fjodor Dostojevski', 'Amazing Book, about Ana.'),
 (25950724, 'testBook', 'autor', 'decs'),
 (38996773, 'seg', 'esr', 'svd'),
+(66275444, 'RazmjenaMarinoNjegova', 'MarinoTestRazmjene', 'Marinova knjiga'),
+(71878064, 'nova', 'nova', 'nova'),
 (85338843, 'Knjiga od korisnika TEST', 'Testiramo za kupovinu', 'Testiramo kupovinu knjiga.'),
 (91886543, 'test', 'test', 'test'),
 (112270560, 'test', 'te', 'test'),
+(118325091, 'marino', 'marino', 'marino'),
 (123102973, 'test', 'test', 'test'),
 (125982519, 'test', 'test', 'test'),
 (135544289, 'asd', 'ad', 'asd'),
@@ -59,8 +62,10 @@ INSERT INTO `knjige` (`idKnjige`, `naziv_knjige`, `autor`, `Description`) VALUES
 (227475030, 'how to love', 'Stura marino', 'volim luku'),
 (228499805, 'test', 'test', 'test'),
 (240508263, 'dsa', 'dsa', 'dsa'),
+(241465375, 'krseva', 'krseva', 'krseav'),
 (247895104, 'Moja igra', 'Luka Modric', 'Moja igra is written by the talented Croatian fo..'),
 (256534695, 'tt', 'tt', 'tt'),
+(282149275, 'Johnova', 'JohnDoeKnjiga', 'JohnovaKnjigaDoe'),
 (284298740, 'dvsvsd', 'fewwef', 'gwegwed'),
 (300380050, 'Moj program', 'Marino Štura', 'Knjiga govori o prvom programu'),
 (384475309, 'e', 'e', 'e'),
@@ -72,6 +77,7 @@ INSERT INTO `knjige` (`idKnjige`, `naziv_knjige`, `autor`, `Description`) VALUES
 (423328433, 'test', 'test', 'test'),
 (431630138, 't', 't', 't'),
 (468530946, 'test', 'test', 'test'),
+(474854222, 'bac', 'bac', 'bac'),
 (484525344, 'Napredno programiranje 3', 'Alen Lovrencic', 'Napredni koncepti programiranja'),
 (486662880, 'asd', 'asd', 'asdasfasfsafsafasfasfsafgdahfdhrgjztzfsdgsdfsdhfgsdfasfsdgdsgffsafsdgdfsgsddsagddasdgsdfdshdfsdbsdsrbrdhfadfdsggea'),
 (510190237, 'test', 'testreq.params.id', 'test'),
@@ -108,18 +114,14 @@ CREATE TABLE `knjizara_has_knjige` (
   `Knjige_idKnjige` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------;
+-- --------------------------------------------------------
 
-
-INSERT INTO `knjizara_has_knjige` (`knjizara_has_knjige_id`, `Knjizara_idKnjizara`, `Knjige_idKnjige`) VALUES
-(1, 1, 1690785),
-(2,1, 85338843),
-(,2, 152047813),
 --
 -- Table structure for table `purchased_books`
 --
 
 CREATE TABLE `purchased_books` (
+  `id_purchased_book` int(11) NOT NULL,
   `idKnjige` int(11) NOT NULL,
   `id_user` int(11) DEFAULT NULL,
   `naziv_knjige` varchar(255) DEFAULT NULL,
@@ -131,17 +133,13 @@ CREATE TABLE `purchased_books` (
 -- Dumping data for table `purchased_books`
 --
 
-INSERT INTO `purchased_books` (`idKnjige`, `id_user`, `naziv_knjige`, `autor`, `Description`) VALUES
-(6, 1, 'Tired of Winning', 'Guide on how to win', 'Donald Trump'),
-(15348240, 7, 'Zlocin i kazna', 'Man who killed grandma, and was emotional', 'Fjodor Miljic Dostojevski'),
-(16907856, 7, 'Ana Karenjina', 'Amazing Book, about Ana.', 'Fjodor Dostojevski'),
-(85338843, 7, 'Knjiga od korisnika TEST', 'Testiramo kupovinu knjiga.', 'Testiramo za kupovinu'),
-(152047813, 1, 'Uvod u algoritme', 'Uvod u sve vrste algoritama', 'Alen Lovrenic'),
-(227475030, 7, 'how to love', 'volim luku', 'Stura marino'),
-(240508263, 1, 'dsa', 'dsa', 'dsa'),
-(247895104, 7, 'Moja igra', 'Moja igra is written by the talented Croatian fo..', 'Luka Modric'),
-(484525344, 1, 'Napredno programiranje 3', 'Napredni koncepti programiranja', 'Alen Lovrencic'),
-(486662880, 1, 'asd', 'asdasfasfsafsafasfasfsafgdahfdhrgjztzfsdgsdfsdhfgsdfasfsdgdsgffsafsdgdfsgsddsagddasdgsdfdshdfsdbsdsrbrdhfadfdsggea', 'asd');
+INSERT INTO `purchased_books` (`id_purchased_book`, `idKnjige`, `id_user`, `naziv_knjige`, `autor`, `Description`) VALUES
+(11, 227475030, 1, 'how to love', 'volim luku', 'Stura marino'),
+(12, 227475030, 5, 'how to love', 'volim luku', 'Stura marino'),
+(13, 227475030, 1, 'how to love', 'volim luku', 'Stura marino'),
+(14, 15348240, 5, 'Zlocin i kazna', 'Man who killed grandma, and was emotional', 'Fjodor Miljic Dostojevski'),
+(15, 16907856, 5, 'Ana Karenjina', 'Amazing Book, about Ana.', 'Fjodor Dostojevski'),
+(16, 15348240, 1, 'Zlocin i kazna', 'Man who killed grandma, and was emotional', 'Fjodor Miljic Dostojevski');
 
 -- --------------------------------------------------------
 
@@ -180,11 +178,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `username`, `email`, `password`, `create_time`, `address`, `first_name`, `last_name`, `money`, `status`) VALUES
-(1, 'john_doe', 'john.doe@example.com', '1234', '2023-11-05 15:00:42', '123 Main St, City, Country', 'John ', 'Doe', 499, 'I like reading a lot'),
+(1, 'john_doe', 'john.doe@example.com', '1234', '2023-11-05 15:00:42', '123 Main St, City, Country', 'John ', 'Doe', 199, 'I like reading a lot'),
 (2, 'jane_smith', 'jane.smith@example.com', 'password456', '2023-11-05 15:00:42', '456 Elm St, Town, Country', 'Jane ', 'Smith', 135, 'Reading is my favourite hobby'),
 (3, 'joey_balboa', 'yoe_te@gmail.com', '12345', '2023-11-17 19:47:07', '765 Royal Lane, USA', 'Joe', 'Balboa', 0, 'I like books and the gym'),
 (4, 'test', 'test', 'test', '2023-11-18 14:38:35', 'test', 'test', 'test', 75, NULL),
-(5, 'marino', 'mojmail@gmail.com', '1234', '2023-11-18 14:44:39', 'Sv. Bana jelacica 5', 'Marino', 'Štura', 50, NULL),
+(5, 'marino', 'mojmail@gmail.com', '1234', '2023-11-18 14:44:39', 'Sv. Bana jelacica 5', 'Marino', 'Štura', 1, NULL),
 (7, 'test', 'test', 'test', '2023-11-18 14:47:27', 'test', 'test', 'test', 300, NULL),
 (8, 'test', 'test', 'ttest', '2023-11-18 15:21:34', 'test', 'test', 'test', NULL, NULL),
 (9, 'tst', 'test', 'test', '2023-11-18 15:23:33', 'test', 'test', 'test', NULL, NULL),
@@ -216,18 +214,19 @@ CREATE TABLE `user_has_knjige` (
 --
 
 INSERT INTO `user_has_knjige` (`user_id_user`, `Knjige_idKnjige`) VALUES
-(1, 16907856),
-(1, 85338843),
+(1, 15348240),
+(1, 71878064),
 (1, 152047813),
 (1, 227475030),
-(1, 240508263),
-(1, 247895104),
+(1, 241465375),
+(1, 474854222),
 (1, 484525344),
-(1, 486662880),
-(7, 15348240);
-
-
-
+(5, 15348240),
+(5, 118325091),
+(5, 241465375),
+(5, 474854222),
+(5, 486662880),
+(14, 241465375);
 
 -- --------------------------------------------------------
 
@@ -269,7 +268,7 @@ ALTER TABLE `knjizara_has_knjige`
 -- Indexes for table `purchased_books`
 --
 ALTER TABLE `purchased_books`
-  ADD PRIMARY KEY (`idKnjige`);
+  ADD PRIMARY KEY (`id_purchased_book`);
 
 --
 -- Indexes for table `reviews`
@@ -322,6 +321,12 @@ ALTER TABLE `knjizara`
 --
 ALTER TABLE `knjizara_has_knjige`
   MODIFY `knjizara_has_knjige_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `purchased_books`
+--
+ALTER TABLE `purchased_books`
+  MODIFY `id_purchased_book` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `reviews`
